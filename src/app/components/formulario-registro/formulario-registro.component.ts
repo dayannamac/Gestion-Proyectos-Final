33dd45bbeb1db08b95d07fc9585194c253d60b63
leonardoAL124 <leonardoAL124@users.noreply.github.com>
2024-07-14 10:33:47 -0500

    Registro - login

--- src/app/components/formulario-registro/formulario-registro.component.ts
+++ src/app/components/formulario-registro/formulario-registro.component.ts
@@ -13,18 +13,36 @@ export class FormularioRegistroComponent {
 
   constructor(private servicio: UsuariosService) {}
 
+  idUsuario: any;
   nombreCompleto: any;
   usuario: any;
   password: any;
 
   saveUsers(formulario: any) {
 
+    this.servicio.getUsuarios().subscribe((users) => {
+      let idMayor = 0;
+
+      for (const user of users) {
+        if (idMayor != 0) {
+          if (user.id >= idMayor) {
+            idMayor = user.id;
+          }
+        } else {
+          idMayor = user.id
+        }
+      }
+
+      this.idUsuario = idMayor + 1;
+    });
+
     const temp = {
-      nombreCompleto: formulario.value.nombre,
+      id: this.idUsuario,
+      nombre: formulario.value.nombre,
       usuario: formulario.value.usuario,
       password: formulario.value.password,
     }
-    //this.servicio.postUsers(temp).subscribe()
+    this.servicio.postUsuarios(temp).subscribe()
 
     alert('usuario registrado!')
     window.location.reload()
